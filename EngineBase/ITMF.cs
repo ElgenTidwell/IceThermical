5a18dfa7b24e75b13af2b52b4164de4ae976faa8
Elgen T <53947938+ElgenTidwell@users.noreply.github.com>
2022-07-10 13:24:09 -0600

    Updated the map renderer.

--- EngineBase/ITMF.cs
+++ EngineBase/ITMF.cs
@@ -11,7 +11,7 @@ namespace IceThermical.Map
 {
 	public class LoadedITM
 	{
-		List<VertexPositionNormalTexture> vertices;
+		List<Tuple<BoundingBox,VertexPositionNormalTexture[]>> brushes = new List<Tuple<BoundingBox, VertexPositionNormalTexture[]>>();
 
 		public void LoadMap(string path)
 		{
@@ -35,10 +35,34 @@ namespace IceThermical.Map
 
 				if (brush.invis) continue;
 
-				foreach(int tri in brush.triangles)
+				BoundingBox bbox = new BoundingBox();
+				bbox.Min = new Vector3(brush.centerX, brush.centerY, brush.centerZ) - new Vector3(brush.height / 2, brush.length / 2, brush.width / 2);
+				bbox.Max = new Vector3(brush.centerX, brush.centerY, brush.centerZ) + new Vector3(brush.height / 2, brush.length / 2, brush.width / 2);
+
+				VertexPositionNormalTexture[] verts=new VertexPositionNormalTexture[brush.triangles.Length];
+				int i = 0;
+				foreach (int tri in brush.triangles)
 				{
-					vertices.Add(new VertexPositionNormalTexture(brush.vertices[tri], brush.normals[tri], brush.uvs[tri]));
+					verts[i] = new VertexPositionNormalTexture(brush.vertices[tri], brush.normals[tri], brush.uvs[tri]);
+					i++;
 				}
+				brushes.Add(Tuple.Create(bbox,verts));
+			}
+		}
+		public void RenderMap(Camera cam,GraphicsDevice gd)
+		{
+			if (brushes.Count == 0) return;
+
+			for(int i = 0; i < brushes.Count; i ++)
+			{
+				if (Vector3.Dot(cam.camPos - brushes[i].Item1.Min, cam.forward) < 1) continue;
+
+				VertexBuffer buffer = new VertexBuffer(gd, typeof(VertexPositionNormalTexture),brushes[i].Item2.Length,BufferUsage.WriteOnly);
+				buffer.SetData(brushes[i].Item2);
+
+				gd.SetVertexBuffer(buffer);
+				gd.DrawPrimitives(PrimitiveType.TriangleList,0,buffer.VertexCount);
+				gd.SetVertexBuffer(null);
 			}
 		}
 	}
